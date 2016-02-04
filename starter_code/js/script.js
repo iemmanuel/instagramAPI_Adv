620a31fc466babb9dfe585477e9960c59bb28bb8
iemmanuel <iemmanuel@cristoreyny.org>
2016-02-04 13:48:58 +0000

    flickr api ian

--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -1,6 +1,20 @@
 // YOUR CODE GOES HERE
-var clientID = "REPLACE ME WITH CLIENT ID";
-
+var clientID = "3565fe59fc166cb7f5a0ab9b7c982cc8";
+var userInput = "";
 $(document).ready(function() {
-    // your code here
+    $("#search-btn").click(function() {
+        userInput = $("#search-query").val();
+        searchFlickr(clientID, userInput, 10, function(photos) {
+            $('#search-results').empty('#search-results');
+            for (var i = 0; i < photos.photo.length; i++)
+                $('#search-results').append('<img src=' + photos.photo[i].url_m + '>');
+                
+        });
+        // your code here
+    });
+
+
+    $("#reset-btn").click(function() {
+        $('#search-results').empty('#search-results');
+    })
 });
